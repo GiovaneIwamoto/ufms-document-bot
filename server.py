b996a876dbd0d7f7d31118785d35f8f4c2ca8a9f
mtguerson <mtguerson@gmail.com>
2024-09-26 15:40:58 -0400

    fix: add telegram bot token to an env

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
+
+load_dotenv()
+
+token = os.getenv('TELEGRAM_BOT_TOKEN')
 
 async def start(update: Update, context) -> None:
     await update.message.reply_text('Olá! Eu sou um bot básico no Telegram.')
@@ -8,9 +14,6 @@ async def help_command(update: Update, context) -> None:
     await update.message.reply_text('Aqui estão os comandos disponíveis:\n/start - Iniciar\n/help - Obter ajuda')
 
 if __name__ == '__main__':
-    # Substitua 'YOUR_TOKEN' pelo token do seu bot que você obteve no BotFather
-    token = '7852316944:AAHuHKYWD9DhzDrNYgY09HwDQDG_AqCBclo'
-    
     application = ApplicationBuilder().token(token).build()
 
     application.add_handler(CommandHandler('start', start))
